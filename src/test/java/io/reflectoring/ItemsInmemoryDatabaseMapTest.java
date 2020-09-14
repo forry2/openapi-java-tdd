c912e010ffebb2390252c9f195728630327e5a31
Roberto Bruni <roberto.bruni@par-tec.it>
2020-09-14 02:47:45 +0200

    implemented one test for StatusApiDelegateImpl class

--- src/test/java/io/reflectoring/ItemsInmemoryDatabaseMapTest.java
+++ src/test/java/io/reflectoring/ItemsInmemoryDatabaseMapTest.java
@@ -1,5 +1,7 @@
 package io.reflectoring;
 
+import org.springframework.boot.test.context.SpringBootTest;
+
 @SpringBootTest
 public class ItemsInmemoryDatabaseMapTest {
 }
--- src/test/java/io/reflectoring/StatusApiDelegateImplTest.java
+++ src/test/java/io/reflectoring/StatusApiDelegateImplTest.java
@@ -1,7 +1,20 @@
 package io.reflectoring;
 
+import io.reflectoring.service.StatusApiDelegateImpl;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Assertions.*;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 public class StatusApiDelegateImplTest {
+    @Test
+    public void tddTest() {
+        StatusApiDelegateImpl x = new StatusApiDelegateImpl();
+        ResponseEntity responseEntity = x.apiGetStatus();
+        assertTrue(responseEntity.getBody() != null && x.apiGetStatus().getBody().getStatus() < 10);
+    }
 }
